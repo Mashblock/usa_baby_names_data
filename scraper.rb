e8b105611311b3e993617d780b594db54edb76d7
Cameron Prebble <cameron.prebble@abletech.co.nz>
2015-12-06 18:12:46 +0100

    Add scraper

--- scraper.rb
+++ scraper.rb
@@ -1,25 +1,28 @@
-# This is a template for a Ruby scraper on morph.io (https://morph.io)
-# including some code snippets below that you should find helpful
+require 'scraperwiki'
+require 'net/http'
+require 'nokogiri'
 
-# require 'scraperwiki'
-# require 'mechanize'
-#
-# agent = Mechanize.new
-#
-# # Read in a page
-# page = agent.get("http://foo.com")
-#
-# # Find somehing on the page using css selectors
-# p page.at('div.content')
-#
-# # Write out to the sqlite database using scraperwiki library
-# ScraperWiki.save_sqlite(["name"], {"name" => "susan", "occupation" => "software developer"})
-#
-# # An arbitrary query against the database
-# ScraperWiki.select("* from data where 'name'='peter'")
+(1998..2014).each do |year|
+  uri = URI('https://www.ssa.gov/cgi-bin/popularnames.cgi')
+  res = Net::HTTP.post_form(uri, year: year, top: 100, number: 'n')
+  doc = Nokogiri::HTML(res.body)
 
-# You don't have to do things with the Mechanize or ScraperWiki libraries.
-# You can use whatever gems you want: https://morph.io/documentation/ruby
-# All that matters is that your final data is written to an SQLite database
-# called "data.sqlite" in the current working directory which has at least a table
-# called "data".
+  doc.search("table[summary='Popularity for top 100'] tr[align='right']").each do |row|
+    cells = row.search("td")
+    ScraperWiki.save_sqlite([:name, :year, :gender], {
+      gender: 'male',
+      year: year,
+      rank: cells[0].text.to_i,
+      name: cells[1].text.to_s,
+      number: cells[2].text.gsub!(',','').to_i
+    })
+
+    ScraperWiki.save_sqlite([:name, :year, :gender], {
+      gender: 'female',
+      year: year,
+      rank: cells[0].text.to_i,
+      name: cells[3].text.to_s,
+      number: cells[4].text.gsub!(',','').to_i
+    })
+  end
+end
